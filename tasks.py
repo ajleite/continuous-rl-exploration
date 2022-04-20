071dd1b5066581100409f979bd5ba47940455cf3
Abe Leite <abrahamjleite@gmail.com>
2022-04-20 14:51:05 -0400

    Finally eliminated scaling!

--- tasks.py
+++ tasks.py
@@ -102,8 +102,8 @@ class HalfCheetahVAETask:
 
         self.env = gym.make("HalfCheetahMuJoCoEnv-v0")
         self.env.seed(int(rng.integers(2**63-1)))
-        self.env._render_width = 96
-        self.env._render_height = 96
+        self.env.env._render_width = 96
+        self.env.env._render_height = 96
 
         self.obs_shape = (self.vae.latent_dim,)
         self.action_shape = (6,)
@@ -117,8 +117,6 @@ class HalfCheetahVAETask:
         return self.env.reset()
     def step(self, action):
         _, r, t, _ = self.env.step(action)
-        self.env._render_width = 96
-        self.env._render_height = 96
         vis_obs = self.env.render('rgb_array')
         self.im_buffer[self.im_buffer_i] = vis_obs
         self.im_buffer_i += 1
